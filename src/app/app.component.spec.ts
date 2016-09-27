5d275b8f899598135cb42e86280d04ddd2dc35dc
Norman <ua.norman@gmail.com>
2016-09-27 12:30:12 +0200

    RouterLink Outlet was tested.
    - Test for testing RouterLink Outlet were added to app.component.spec
    - Routes were change in app.component.html

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,8 +8,12 @@ describe('Component: Dashboard', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
   let element: DebugElement;
+  let linkDes;
+  let links;
 
   describe('AppComponent', () => {
+
+    //Configure and creates the component
     beforeEach(() => {
 
       TestBed.configureTestingModule({
@@ -28,9 +32,43 @@ describe('Component: Dashboard', () => {
 
     });
 
+    //Getting the RouterOutlet links
+    beforeEach(() => {
+      // trigger initial data binding
+      fixture.detectChanges();
+
+      // find DebugElements with an attached RouterLinkStubDirective
+      linkDes = fixture.debugElement
+        .queryAll(By.directive(RouterLinkStubDirective));
+
+      // get the attached link directive instances using the DebugElement injectors
+      links = linkDes
+        .map(de => de.injector.get(RouterLinkStubDirective) as RouterLinkStubDirective);
+    });
+
     it('should display component title', () => {
         fixture.detectChanges(); // trigger data binding
         expect(element.nativeElement.textContent).toContain(component.title);
     });
+
+    it('can get RouterLinks from template', () => {
+      expect(links.length).toBe(2, 'should have 2 links');
+      expect(links[0].linkParams).toBe('/dashboard');
+      expect(links[1].linkParams).toBe('/tasks/new');
+    });
+
+    it('can click Heroes link in template', () => {
+      const addTaskLinkDe = linkDes[1];
+      const addTaskLink = links[1];
+
+      expect(addTaskLink.navigatedTo).toBeNull();
+
+      addTaskLinkDe.triggerEventHandler('click', null);
+      fixture.detectChanges();
+
+      expect(addTaskLink.navigatedTo).toBe('/tasks/new');
+    });
+
+
   });
 });
